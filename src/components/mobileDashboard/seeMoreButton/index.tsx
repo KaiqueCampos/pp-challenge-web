b499879026e42a89f6060e72f40931909da4a9a0
Kaique Campos <kaiquecamposferreira@gmail.com>
2022-03-12 12:38:13 -0300

    see more button in mobile dashboard ready

--- src/components/mobileDashboard/seeMoreButton/index.tsx
+++ src/components/mobileDashboard/seeMoreButton/index.tsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react"
 import { Button } from "./styles"
 
+type toggleSeeMore = {
+    element: HTMLElement;
+    event: MouseEvent;
+}
+
 export function SeeMoreButton() {
+
+    const [elementIsActive, setElementIsActive] = useState<HTMLElement | null>()
+
+
+    function toggleSeeMore(e: React.MouseEvent<HTMLButtonElement>) {
+
+        // Set Element div is actived or not
+        const element = e.currentTarget.parentElement
+        element !== null ? (element?.id === "active" ? (element.id = "inactive") : (element.id = "active")) : ""
+
+        // change the img icon if div is activated or not
+        const iconToChange = element?.getElementsByTagName('img')[2]
+
+        console.log(iconToChange)
+
+        if (iconToChange !== undefined) {
+            iconToChange.src = element?.id === 'active' ? './seeMoreUp.svg' : './seeMoreDown.svg'
+        }
+
+    }
+
     return (
         <Button
-            onClick={(e) => {
-                const element = e.currentTarget.parentElement
-                element !== null ? (element?.id === "active" ? (element.id = "inactive") : (element.id = "active")) : ""
-            }}
+            id="SeeMoreButton"
+            onClick={(e) => toggleSeeMore(e)}
         >
-            <img src="./seeMore.svg" />
+
+            <img src="./seeMoreDown.svg"/>
+
         </Button>
     )
 }
